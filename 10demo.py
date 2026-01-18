b3f2ef6b3dec51b83399303956f060e6ce03d337
cjhpark <cjhpark@ucdavis.edu>
2026-01-18 11:58:45 -0800

    10demo.py

--- 10demo.py
+++ 10demo.py
@@ -1,1 +1,21 @@
+#10demo.py Chris Park
+import math
 print("hello, again")
+
+#Scientific Notation
+print(1.5e-2)
+
+#Math Operators
+print(1+2)
+print(1-2)
+print(2*2)
+print(2/1)
+print(2**2)
+print(5//4)
+print(10%4)
+print(3*(1+2))
+print(math.ceil(5.2))
+print(math.log2(8))
+print(math.sqrt(9))
+print(math.factorial(5))
+print(math.e)
