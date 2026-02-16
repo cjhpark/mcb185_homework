bf244c756b4c6bd26909f3c7b611a82d82f90926
cjhpark <cjhpark@ucdavis.edu>
2026-02-16 11:07:30 -0800

    homework

---
+++ 20Homework/25scoringmatrix.py
@@ -0,0 +1,21 @@
+#25scoringmatrix.py Chris Park
+
+import sys
+import math
+
+alphabet = sys.argv[1]
+split_alph = []
+for l in range(len(alphabet)):
+	split_alph.append(alphabet[l])
+print(split_alph)
+
+matchscore = int(sys.argv[2])
+mismatchscore = int(sys.argv[3])
+
+score = 0
+for i in range(len(alphabet)):
+	matrixscores = []
+	for j in range(len(alphabet)):
+		if alphabet[i] == alphabet[j]:	matrixscores.append(matchscore)
+		if alphabet[i] != alphabet[j]:	matrixscores.append(mismatchscore)
+	print(alphabet[i], matrixscores)
